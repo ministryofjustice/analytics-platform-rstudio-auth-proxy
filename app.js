185a215d8a478fda395572ecb945469968a21d18
Andy Driver <andyhd@users.noreply.github.com>
2017-07-17 11:39:19 +0100

    Revert "Revert "Remove unused header setting code for OS Rstudio""

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(favicon(path.join(__dirname, 'public/databot.png')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
