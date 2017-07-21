9bdda07d652597a778a9536bb159cfb8340d25c2
Andy Driver <andy@pagezero.net>
2017-07-21 11:43:11 +0100

    Bypass RStudio login by sending auth cookies via proxy

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,56 @@ var express = require('express');
 var passport = require('passport');
 var httpProxy = require('http-proxy');
 var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn()
+var crypto = require('crypto');
 var router = express.Router();
+
+
+function formatDate(d) {
+  // Eg: Thu, 01 Aug 2017 07:05:08 GMT
+  var dateString = d.toLocaleString('en-US', {
+    'weekday': 'short', 'day': '2-digit', 'month': 'short', 'year': 'numeric',
+    'hour': 'numeric', 'minute': 'numeric', 'second': 'numeric',
+    'hour12': false, 'timeZone': 'GMT', 'timeZoneName': 'short'
+  });
+  var parts = dateString.split(' ');
+  // swap month and day
+  parts[2] = [parts[1], parts[1] = parts[2]][0];
+  // strip comma from day and year
+  parts[1] = parts[1].slice(0, 2);
+  parts[3] = parts[3].slice(0, 4);
+  return parts.join(' ');
+}
+
+
+function expiry(duration) {
+  var expires = new Date();
+  expires.setTime(expires.getTime() + duration);
+  return expires;
+}
+
+
+function base64HMAC(message, key) {
+  return crypto.createHmac('SHA256', key).update(message).digest('base64')
+}
+
+
+function secureCookie(value, validDuration) {
+  var expires = formatDate(expiry(validDuration));
+  var hmac = base64HMAC(value + expires, process.env.SECURE_COOKIE_KEY);
+  return [value, expires, hmac].map(encodeURIComponent).join('|');
+}
+
+
+function setAuthCookie(req) {
+  var staySignedInDays = process.env.STAY_SIGNED_IN_DAYS || 1;
+  var duration = staySignedInDays * 24 * 60 * 60 * 1000;
+  cookie = [
+    'user-id=' + secureCookie('rstudio', duration),
+    'Path=/',
+    'HttpOnly'
+  ].join('; ');
+  req.setHeader('Cookie', cookie);
+}
 
 var env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
@@ -17,12 +66,17 @@ var proxy = httpProxy.createProxyServer({
     port: process.env.SHINY_PORT
   },
   ws: true,
+  preserveHeaderKeyCase: true,
   proxyTimeout: (24 * 60 * 60 * 1000) // 1 day
 });
 
 proxy.on('error', function(e) {
   console.log('Error connecting');
   console.log(e);
+});
+
+proxy.on('proxyReq', function (proxyReq, req, res, options) {
+  setAuthCookie(proxyReq);
 });
 
 /* Handle login */
@@ -34,6 +88,11 @@ router.get('/login',
 
 /* Handle logout */
 router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/login');
+});
+
+router.post('/auth-sign-out', function (req, res) {
   req.logout();
   res.redirect('/login');
 });
