9d41ab8f6f985a54ec275be0307b72b39aed4138
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-02-13 11:08:15 +0000

    Updated Auth0 scopes: Only 'profile' should be required
    We only use the user nickname to check if it's authorised to
    access RStudio. This field should come from from the `profile` scope.
    
    SEE: https://auth0.com/docs/scopes/current

--- app/config.js
+++ app/config.js
@@ -21,7 +21,7 @@ config.auth0 = {
   clientID: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
   domain: process.env.AUTH0_DOMAIN,
-  scope: 'openid profile',
+  scope: 'profile',
   passReqToCallback: true,
 };
 
