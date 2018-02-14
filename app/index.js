a98cf4784c85a2cc44c77dc96b297ec1542c69d3
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-02-14 15:04:45 +0000

    Linted app/index.js

--- app/index.js
+++ app/index.js
@@ -1,25 +1,29 @@
 require('dotenv').config();
-var bole = require('bole');
-var config = require('./config');
-var morgan = require('morgan');
-var passport = require('passport');
-var path = require('path');
+const bole = require('bole');
+const express = require('express');
+const morgan = require('morgan');
+const passport = require('passport');
+const path = require('path');
 const Auth0Strategy = require('passport-auth0-openidconnect').Strategy;
 
+const config = require('./config');
+const errors = require('./errors');
+const routes = require('./routes');
 
-var app = require('express')();
+
+const app = express();
 app.set('views', path.join(__dirname, 'views'));
 
 // add before logging to avoid favicon requests in logs
 app.use(require('serve-favicon')(path.join(__dirname, 'favicon.ico')));
 
-bole.output({level: config.log.level, stream: process.stdout});
+bole.output({ level: config.log.level, stream: process.stdout });
 app.use(morgan('combined'));
 
 app.use(require('cookie-parser')());
 app.use(require('express-session')(config.session));
 
-strategy = new Auth0Strategy(
+const strategy = new Auth0Strategy(
   config.auth0,
   ((req, issuer, audience, profile, accessToken, refreshToken, params, callback) => {
     req.session.id_token = params.id_token;
@@ -29,19 +33,19 @@ strategy = new Auth0Strategy(
 );
 passport.use(strategy);
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser((user, done) => {
   done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
+passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(require('./routes'));
-app.use(require('./errors'));
+app.use(routes);
+app.use(errors);
 
 
 module.exports = app;
