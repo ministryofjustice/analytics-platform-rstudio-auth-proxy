d92bc2034c88ca1e8a75fdbd178642827d3dc328
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-02-14 14:42:53 +0000

    Removed unused 'nunjucks' and views/layout.html

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 var bole = require('bole');
 var config = require('./config');
 var morgan = require('morgan');
-var nunjucks = require('nunjucks');
 var passport = require('passport');
 var path = require('path');
 const Auth0Strategy = require('passport-auth0-openidconnect').Strategy;
@@ -16,11 +15,6 @@ app.use(require('serve-favicon')(path.join(__dirname, 'favicon.ico')));
 
 bole.output({level: config.log.level, stream: process.stdout});
 app.use(morgan('combined'));
-
-nunjucks.configure(path.join(__dirname, 'views'), {
-  autoescape: true,
-  express: app
-});
 
 app.use(require('cookie-parser')());
 app.use(require('express-session')(config.session));
