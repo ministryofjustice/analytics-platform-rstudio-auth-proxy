b7cb8f5c90fff4cabc58c5cb6d1f7fc888ece73a
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2018-02-14 15:15:40 +0000

    Linted app/routes.js (mostly)

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,20 @@
-var config = require('./config');
-var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
-var express = require('express');
-var authorization = require('./middleware/authorization');
-var passport = require('passport');
-var proxy = require('./proxy');
-var router = new express.Router();
+const { ensureLoggedIn } = require('connect-ensure-login');
+const express = require('express');
+const passport = require('passport');
 const uuidv4 = require('uuid/v4');
 
+const config = require('./config');
+const authorization = require('./middleware/authorization');
+const proxy = require('./proxy');
+
+
+const router = new express.Router();
 
 const RETURN_TO = encodeURI(`${config.app.protocol}://${config.app.host}`);
 const SSO_LOGOUT_URL = `https://${config.auth0.domain}${config.auth0.sso_logout_url}?returnTo=${RETURN_TO}&client_id=${config.auth0.clientID}`;
 
 
-router.get('/login', function(req, res, next) {
+router.get('/login', (req, res, next) => {
   if (req.isAuthenticated()) {
     if (/^http/.test(req.session.returnTo)) {
       res.send(400, 'URL must be relative');
@@ -39,28 +41,28 @@ router.get('/auth-sign-out', logout);
 
 router.get('/callback', [
   passport.authenticate('auth0-oidc', { failureRedirect: '/login' }),
-  function(req, res) {
+  (req, res) => {
     res.redirect(req.session.returnTo || '/');
-  }
+  },
 ]);
 
 router.all(/.*/, [
   ensureLoggedIn('/login'),
   authorization,
-  function(req, res) {
-    parseBody(req).then(function (body) {
+  (req, res) => {
+    parseBody(req).then((body) => {
       req.body = body;
       proxy.web(req, res);
     });
-  }
+  },
 ]);
 
 function parseBody(req) {
-  var body = [];
-  return new Promise(function (resolve, reject) {
-    req.on('data', function (chunk) { body.push(chunk); });
-    req.on('end', function () { resolve(body); });
-    req.on('error', function (err) { reject(err); });
+  let body = [];
+  return new Promise((resolve, reject) => {
+    req.on('data', (chunk) => { body.push(chunk); });
+    req.on('end', () => { resolve(body); });
+    req.on('error', (err) => { reject(err); });
   });
 }
 
